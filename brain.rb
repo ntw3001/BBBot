9ba4c9898ae303ba90c3575e4669a78f83cfe981
Nick Westbrook <ntw3001@gmail.com>
2024-04-11 02:32:36 +0900

    trying again

--- brain.rb
+++ brain.rb
@@ -1,22 +1,3 @@
-# app.rb
-require 'sinatra'
-require 'json'
-require 'net/http'
-require 'uri'
-require 'tempfile'
-require 'line/bot'
-
-require_relative 'imagga'
-require_relative 'weather_api'
-require_relative 'tokyo_events_api'
-
-def client
-  @client ||= Line::Bot::Client.new do |config|
-    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-    config.channel_token = ENV['LINE_ACCESS_TOKEN']
-  end
-end
-
 def bot_answer_to(a_question)
   if a_question.match?("weather")
     "2: Sweltering heat
@@ -77,78 +58,6 @@ def bot_answer_to(a_question)
     "Please just say Spring, Summer, Autumn, Winter, or Weather"
   end
 end
+a_question = gets.chomp
 
-def send_bot_message(message, client, event)
-  # Log prints for debugging
-  p 'Bot message sent!'
-  p event['replyToken']
-  p client
-
-  message = { type: 'text', text: message }
-  p message
-
-  client.reply_message(event['replyToken'], message)
-  'OK'
-end
-
-get '/' do
-  "Up and running!"
-end
-
-post '/callback' do
-  body = request.body.read
-
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
-
-  events = client.parse_events_from(body)
-  events.each do |event|
-    p event
-    # Focus on the message events (including text, image, emoji, vocal.. messages)
-    next if event.class != Line::Bot::Event::Message
-
-    case event.type
-    # when receive a text message
-    when Line::Bot::Event::MessageType::Text
-      user_id = event['source']['userId']
-      response = client.get_profile(user_id)
-      if response.class == Net::HTTPOK
-        contact = JSON.parse(response.body)
-        p contact
-      else
-        # Can't retrieve the contact info
-        p "#{response.code} #{response.body}"
-      end
-
-      if event.message['text'].downcase == 'hello, world'
-        # Sending a message when LINE tries to verify the webhook
-        send_bot_message(
-          'Everything is working!',
-          client,
-          event
-        )
-      else
-        # The answer mechanism is here!
-        send_bot_message(
-          bot_answer_to(event.message['text']),
-          client,
-          event
-        )
-      end
-      # when receive an image message
-    when Line::Bot::Event::MessageType::Image
-      response_image = client.get_message_content(event.message['id'])
-      fetch_imagga(response_image) do |image_results|
-        # Sending the image results
-        send_bot_message(
-          "Looking at that picture, the first words that come to me are #{image_results[0..1].join(', ')} and #{image_results[2]}. Pretty good, eh?",
-          client,
-          event
-        )
-      end
-    end
-  end
-  'OK'
-end
+puts bot_answer_to(a_question)
