fce7f712bcd420b9f119b503a983b81008a4cecc
Nick Westbrook <ntw3001@gmail.com>
2024-04-12 01:31:29 +0900

    what if I did it sideways like this

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ require 'uri'
 require 'tempfile'
 require 'line/bot'
 
-require_relative 'imagga'
-require_relative 'weather_api'
-require_relative 'tokyo_events_api'
-
 def client
   @client ||= Line::Bot::Client.new do |config|
     config.channel_secret = ENV['LINE_CHANNEL_SECRET']
@@ -133,17 +129,6 @@ post '/callback' do
         # The answer mechanism is here!
         send_bot_message(
           bot_answer_to(event.message['text']),
-          client,
-          event
-        )
-      end
-      # when receive an image message
-    when Line::Bot::Event::MessageType::Image
-      response_image = client.get_message_content(event.message['id'])
-      fetch_imagga(response_image) do |image_results|
-        # Sending the image results
-        send_bot_message(
-          "Looking at that picture, the first words that come to me are #{image_results[0..1].join(', ')} and #{image_results[2]}. Pretty good, eh?",
           client,
           event
         )
