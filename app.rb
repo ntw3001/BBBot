5203bc524a9bacc45e4930f033b2b813eb12b23c
Nick Westbrook <ntw3001@gmail.com>
2024-09-19 09:37:37 +0900

    .

--- app.rb
+++ app.rb
@@ -193,6 +193,8 @@ def bot_answer_to(table, roll)
       "2-7: Stunned\nPlace the player face down in the sqaure they are occupying. They may not be activated.\nAt the end of each turn, all players on the active team who began the turn stunned become prone.\n\nStunty players are not stunned on a 7, but are instead knocked out (result 8-9).\nPlayers with both Stunty and Thick Skull are stunned on a 7 as normal.\n\n8-9: Knocked out\nRemove the player from the pitch and place them in the KO'd box of their coach's dugout. At the end of each drive, roll a d6 for each KO'd player:\n\nD6 TABLE\n1-3: The player is yet unable to take to the field.\n4-6: The player has recovered and returns to the reserves box. They may be set up for the next drive.\n\nStunty players are KO'd on a result of 7-8, and on a 9 are instead Badly Hurt (as though they had rolled 1-6 on the Casualty table).\nPlayers with Thick Skull are stunned on an 8 and only KO'd on a 9.\nPlayers with both Stunty and Thick Skull are stunned on a 7, KO'd on an 8, and Badly Hurt on a 9.\n\n10-12: Casualty!\nRemove the player from the pitch and place them in the Casualty box of their coach's dugout. Roll on the casualty table to determine the nature of the injury."
     end
     response << injury_response
+
+
   when "amazon", "lizardmen", "slann", "kislev", "kislevcircus"
     available_stars = find_available_stars(stars, roll, "Lustrian Superleague", "Any")
     if available_stars.nil? || available_stars.empty?
@@ -527,14 +529,17 @@ stars = [
 ]
 
 def find_available_stars(stars, roll, *rules)
+  roll = roll.to_i
   available_stars = stars.select do |star|
-    star[:cost] <= roll && rules.any? { |rule| star[:rules].include?(rule) } && !(star[:name] == "Morg 'n' Thorg" && !rules.include?("Sylvanian Spotlight"))
+    star[:cost] <= roll &&
+    rules.any? { |rule| star[:rules].include?(rule) } &&
+    !(star[:name] == "Morg 'n' Thorg" && !rules.include?("Sylvanian Spotlight"))
   end
 
   if available_stars.empty?
-    return []
+    "No available stars for this selection."
   else
-    return available_stars.map { |star| "#{star[:name]}: #{star[:cost]}k." }.join("\n")
+    available_stars.map { |star| "#{star[:name]}: #{star[:cost]}k" }.join("\n")
   end
 
 end
